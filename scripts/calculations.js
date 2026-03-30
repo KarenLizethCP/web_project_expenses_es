4b7ccbd606d80219dee5309258dc0c692d444653
Karen Caceres <karen_lizeth789@hotmail.com>
2026-03-30 17:55:27 +1100

    add initial vbles and array

---
+++ scripts/calculations.js
@@ -0,0 +1,12 @@
+let budgetValue = 0;
+let totalExpensesValue = 0;
+
+const expensesEntries = [
+  ["groceries", 33],
+  ["restaurants", 50],
+  ["transport", 12],
+  ["home", 70],
+  ["subscriptions", 14],
+  ["groceries", 28],
+  ["subscriptions", 12],
+];
