54efef046250a15f07e09c8e345a87cf809dfe6c
Karen Caceres <karen_lizeth789@hotmail.com>
2026-03-30 20:26:49 +1100

    add functions

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,5 +1,6 @@
 let budgetValue = 0;
 let totalExpensesValue = 0;
+let averageExpense = 0;
 
 const expenseEntries = [
   ["groceries", 33],
@@ -17,15 +18,69 @@ for (let i = 0; i < expenseEntries.length; i++) {
 }
 
 function calculateAverageExpense() {
-  if (expenseEntries.length === 0) {
-    console.log("No hay gastos registrados para calcular el promedio.");
-    return;
-  }
-  const averageExpense = totalExpensesValue / expenseEntries.length;
+  averageExpense = totalExpensesValue / expenseEntries.length;
+  return averageExpense;
   console.log(`Gasto promedio: ${averageExpense}`);
+
+  if (expenseEntries.length === 0) {
+    console.log("No hay gastos registrados.");
+  }
 }
 
 function calculateBalance() {
   const balance = budgetValue - totalExpensesValue;
+  return balance;
   console.log(`Balance restante: ${balance}`);
+}
+
+let balanceColor = "green";
+
+function updateBalanceColor() {
+  if (calculateBalance() < 0) {
+    balanceColor = "red";
+  } else if (calculateBalance <= 0.25 * budgetValue) {
+    balanceColor = "orange";
+  } else {
+    balanceColor = "green";
+  }
+}
+
+function calculateCategoryExpenses(category) {
+  let total = 0;
+  for (let i = 0; i < expenseEntries.length; i++) {
+    if (expenseEntries[i][0] === category) {
+      total += expenseEntries[i][1];
+    }
+  }
+  return total;
+  console.log(`Gasto total en ${category}: ${total}`);
+}
+
+function calculateLargestCategory() {
+  const categories = [
+    "groceries",
+    "restaurants",
+    "transport",
+    "home",
+    "subscriptions",
+  ];
+
+  let categoriesData = [];
+
+  for (let i = 0; i < categories.length; i++) {
+    let total = calculateCategoryExpenses(categories[i]);
+    categoriesData.push([categories[i], total]);
+  }
+
+  let largestCategory = categoriesData[0][0];
+  let largestAmount = categoriesData[0][1];
+
+  for (let i = 1; i < categoriesData.length; i++) {
+    if (categoriesData[i][1] > largestAmount) {
+      largestAmount = categoriesData[i][1];
+      largestCategory = categoriesData[i][0];
+    }
+  }
+
+  return largestCategory;
 }
